80683db305b9eb9ba159875edd9378eecb2340e1
soyoungPark <ps.young149@gmail.com>
2023-08-19 00:36:28 +0900

    practice num
    python _ list

--- practice.py
+++ practice.py
@@ -1,53 +1,71 @@
-# 파이썬 시작하기
-# print("Hello World")
-# print("Mary\'s cosmetics")
-# print("신씨가 소리질렀다. \"도둑이야\".")
-# print("C:\\Windows")
-# \n : 줄바꿈
-# \t : 탭
-# print("오늘은", "일요일") # 오늘은 일요일
-# print("naver;kakao;sk;samsung")
-# print("naver/kakao/sk/samsung")
-# print("first", end="");print("second")
-# print(5 / 3)
+# 리스트 []
 
-# 파이썬 변수
-# samsung = 50000
-# all = samsung * 10
-# print(all)
+# 지하철 칸별로 10명, 20명, 30명
+# subway1 = 10
+# subway2 = 20
+# subway3 = 30
 
-# 시가총액 = 298000000000000
-# 현재가 = 50000
-# PER = 15.79
-# print(시가총액, type(시가총액))
-# print(현재가, type(현재가))
-# print(PER, type(PER))
+# subway = [10, 20, 30]
+# print(subway)
 
-# s = "hello"
-# t = "python"
-# print(s + "!", t)
+# subway = ["유재석", "조세호", "박명수"]
+# print(subway)
 
-# print(2 + 2 * 3)
+# #조세호씨가 몇 번째 칸에 타고있는지?
+# print(subway.index("조세호"))
 
-# a = "132"
-# print(type(a))
+# # 하하씨가 다음 정류장에서 다음 칸에 탐
+# subway.append("하하")
+# print(subway)
 
-# num_str = "720"
-# print(str(num_str))
+# # 정형돈씨를 유재석씨와 조세호씨 사이에 태워봅시다
+# subway.insert(1,"정형돈")
+# print(subway)
 
-# num = 100
-# result = str(num)
-# print(result, type(result))
+# #지하철에 있는 사람을 한 명씩 뒤에서 꺼냄
+# print(subway.pop())
+# print(subway)
 
-# num = 15.79
-# print(type(num))
+# # 같은 이름의 사람이 몇 명 있는지 확인
+# subway.append("유재석")
+# print(subway)
+# print(subway.count("유재석"))
 
-# data = "15.79"
-# data = float(data)
-# print(data, type(data))
+# # 정렬도 가능
+# num_list = [5, 2, 4, 3, 1]
+# num_list.sort()
+# print(num_list)
 
-# year = "2020"
-# year = str(year)
-# print(year * 3)
+# #순서 뒤집기 가능
+# num_list.reverse()
+# print(num_list)
 
-print(help)
+# # 모두 지우기
+# num_list.clear()
+# print(num_list)
+
+# # 다양한 자료형 함께 사용
+# num_list = [5,4,2,5,1]
+# mix_list = ["조세호", 20, True]
+# # print(mix_list)
+
+# # 리스트 확장
+# num_list.extend(mix_list)
+# print(num_list)
+
+
+# cabinet = {3:"유재석", 100:"김태호"}
+# print(cabinet[3])
+# print(cabinet[100])
+
+# print(cabinet[5])
+# print(cabinet.get(5))
+# print(cabinet.get(5, "사용 가능"))
+# print("hi")
+
+# print(3 in cabinet)
+# print(5 in cabinet)
+
+cabinet = {"A -3" : "유재석", "B-100":"김태호"}
+print(cabinet["A -3"])
+print(cabinet["B-100"])
---
+++ practice_a.py
@@ -0,0 +1,53 @@
+# 파이썬 시작하기
+# print("Hello World")
+# print("Mary\'s cosmetics")
+# print("신씨가 소리질렀다. \"도둑이야\".")
+# print("C:\\Windows")
+# \n : 줄바꿈
+# \t : 탭
+# print("오늘은", "일요일") # 오늘은 일요일
+# print("naver;kakao;sk;samsung")
+# print("naver/kakao/sk/samsung")
+# print("first", end="");print("second")
+# print(5 / 3)
+
+# 파이썬 변수
+# samsung = 50000
+# all = samsung * 10
+# print(all)
+
+# 시가총액 = 298000000000000
+# 현재가 = 50000
+# PER = 15.79
+# print(시가총액, type(시가총액))
+# print(현재가, type(현재가))
+# print(PER, type(PER))
+
+# s = "hello"
+# t = "python"
+# print(s + "!", t)
+
+# print(2 + 2 * 3)
+
+# a = "132"
+# print(type(a))
+
+# num_str = "720"
+# print(str(num_str))
+
+# num = 100
+# result = str(num)
+# print(result, type(result))
+
+# num = 15.79
+# print(type(num))
+
+# data = "15.79"
+# data = float(data)
+# print(data, type(data))
+
+# year = "2020"
+# year = str(year)
+# print(year * 3)
+
+print(help)
